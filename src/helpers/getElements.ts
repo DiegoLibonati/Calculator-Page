e5f7c5384401bde4d84ec5fcd25b6874a73e5cdc
Die <dlibonati@leafnoise.io>
2025-01-30 13:46:16 -0300

    feat: BEM CSS

--- src/helpers/getElements.ts
+++ src/helpers/getElements.ts
@@ -2,5 +2,5 @@ export const getElements = () => ({
   windowCalculator: document.querySelector(
     ".mirror__window"
   ) as HTMLElement,
-  btnsCalculator: document.querySelectorAll(".button") as NodeList,
+  btnsCalculator: document.querySelectorAll(".actions__action") as NodeList,
 });
